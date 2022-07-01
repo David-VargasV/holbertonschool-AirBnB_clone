928fec279be98a7e4602aa1fc315bc41fb66f88c
David-vargasV <4523@holbertonschool.com>
2022-07-01 18:06:42 -0500

    H_AirBnB_6.0

--- console.py
+++ console.py
@@ -26,5 +26,30 @@ class HBNBCommand(cmd.Cmd):
         """ Empty line does not execute anything """
         return False
 
+    def do_create(self, args):
+        """ Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id. Ex: $ create BaseModel """
+
+    
+    def do_show(self, args):
+        """ Prints the string representation of
+        an instance based on the class name and id """
+
+    
+    def do_destroy(self, args):
+        """ Deletes an instance based on the class name and id """
+
+    
+    def do_all(self, args):
+        """ Prints all string representation of
+        all instances based or not on the class name """
+
+    
+    def do_update(self, args):
+        """ Updates an instance based on the class
+        name and id by adding or updating attribute """
+        
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
