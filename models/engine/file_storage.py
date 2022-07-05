ff16c277577e45e72c1721493ad4b07d2404e15a
root <root@LAPTOP-R1LL42OF.localdomain>
2022-07-04 21:32:59 -0500

    Classes imported

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,10 +2,22 @@
 """ Class FileStorage """
 
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import json
 
 classtype = {
     "BaseModel": BaseModel,
+    "User": User,
+    "Place": Place,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Review": Review,
 }
 
 class FileStorage:
